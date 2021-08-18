36f25d6f8363254baa78d14453c3ac267d516881
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-08-18 19:08:12 -0400

    Practice

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,37 @@
+
+window.alert("WAZZUP");
+var e = "Why"
+window.alert (e);s
+var y ="Stan from Eminem was kinda disturbing"
+document.write(y);
+
+function check() {
+    var content=document.getElementById("Hi").innerText;
+    alert(content); 
+}
+
+//how to write commas without the extra marks showing
+document.write("Lisa told Bart, \ Knock it off")
+
+document.write ("\"Bella Poarch has to be one the most" 
+                + "talented Tik Tok Stars"); 
+
+//concating multiple strings
+var Deities ="Names", First="Krishna", Second="Gaia",
+              Third="Odin", Fourth="Oshun"
+    document.write(First)
+//how to switch font colors and make the string that color
+var reds=("Help");
+var reds=reds.fontcolor("red");
+document.write(reds); 
+//expressions 
+document.write(3 + 3);
+//making a function to click a button and make the 
+//text inside that function whatever color you assigned 
+function func() {
+    var str="This text is purple";
+    var result=str.fontcolor ("purple");
+    document.getElementById ("Purple_Text").innerHTML = result; 
+}
+
+// two slashes lets you comment code
