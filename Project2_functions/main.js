951b5f3b8d208e38547c135176af6a888e31abbd
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-08-19 02:03:43 -0400

    Project 2

---
+++ Project2_functions/main.js
@@ -0,0 +1,12 @@
+//function created to utilize using paragraph element for id
+function p() {
+    var p=("Enter text:");
+    document.getElementById("p1").innerHTML=p; 
+}
+
+//created function using +=
+function myFunc() {
+    var sentence ="I'm learning";
+    sentence += "a lot from this course"; 
+    document.getElementById("Concatenate").innerHTML = sentence; 
+}
