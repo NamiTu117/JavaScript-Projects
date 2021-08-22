57a8670632a494eaf025f8654b35548e27288a7d
NamiTu117 <86804169+NamiTu117@users.noreply.github.com>
2021-08-22 14:23:19 -0400

    Project10

---
+++ Project10_loops_arrays/JS/async.js
@@ -0,0 +1,29 @@
+alert(document.getElementById("p1").innerHTML);
+
+
+//purposeful error 
+function duh() {
+    I = "Always"; 
+    J = " remember: ";
+    document.write(I + J );
+
+    K = " you're  ";
+    L = "blind, ";
+    document.write (K + L);
+
+    M = "not"; 
+    N = " stupid";
+    document.write (M + Z); 
+    document.getElementById("yikes").innerHTML;
+    }
+
+//while loop 
+function count() {
+    var digits = "";
+    var y = 2; 
+    while (y < 9); {
+        digits += "<br>" + y; 
+        y++; 
+    }
+    document.getElementById("counts").innerHTML = digits; 
+}
---
+++ Project10_loops_arrays/JS/main.js
@@ -0,0 +1,63 @@
+function Call_Loop() {
+    var digits = ""; 
+    var x = 2; 
+    while (x < 9); {
+        digits += "<br>" + x;
+        x++; 
+    }
+    document.getElementById("Loop").innerHTML = digits; 
+}
+
+//string length 
+let text = "Hi there"; 
+document.getElementById("lengthy").innerHTML = text.lengthy; 
+
+//for loop 
+var movies = ["John Wick", "The Matrix", "Speed"]; 
+var content = ""; 
+var x;
+function forLoop() {
+    for (x = 0; x < movies.length; x++) {
+        content += movies[x] + "<br>";
+    }
+    document.getElementById("Movies").innerHTML = content; 
+}
+
+function type() {
+    var DogTypes = [];
+    DogTypes[0] ="Rottweiler";
+    DogTypes[1] ="Pitbull";
+    DogTypes[2] = "Siberian Husky";
+    DogTypes[3] = "Doberman"; 
+    document.getElementById("dog").innerHTML = "I'd like to buy a " + DogTypes[2] + " as a pup!"; 
+}
+
+//constant 
+function constant() {
+    const Comics = {book:"Raven", author:"Kami Garcia", year:"2020"};
+    Comics.cover = "soft cover";
+    Comics.price ="$15.00"; 
+    document.getElementById("comic").innerHTML = "This comic book is " + Comics.price + "."; 
+}
+
+let game = {
+    console: "Xbox One",
+    name: "Halo 5: Guardians",
+    year: "2015",
+    rated: "M",
+    description: function() {
+        return "You can only play " + this.name + "on " + this.console; 
+    }
+}; 
+document.getElementById("games").innerHTML = game.description();
+
+//loop w/break statement 
+
+function breakit() {
+let text = ""; 
+for (let k = 0; i < 12; k++) {
+    if (k == 5) {break;}
+    text += "Your number is " + k + "<br>";
+}
+document.getElementById("mystic").innerHTML = text; 
+}
